520c740171a81f17c2cd3ed2258fa9d33cd7fdf5
Nikhil jadav <nikhil.jadhav3210@gmail.com>
2024-12-11 08:48:16 +1100

    Update server.js

--- SIT725-5.2C/server.js
+++ SIT725-5.2C/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const { connectDB } = require('./db');
@@ -7,7 +6,6 @@ const todoController = require('./controllers/todoController');
 const app = express();
 const port = 3000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serve static files from the 'public' folder
 
@@ -20,12 +18,11 @@ app.post('/todos', todoController.addTodo);
 app.put('/todos', todoController.editTodo);
 app.delete('/todos/:id', todoController.removeTodo);
 
-// Serve the index.html when accessing the root
+// Serving the index.html when accessing the root
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
